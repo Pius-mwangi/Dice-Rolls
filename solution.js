7977d0f8522c3f1164404add5a298f670f408e02
PIUS <relimoringa@gmail.com>
2023-10-18 07:23:12 +0300

    .

--- solution.js
+++ solution.js
@@ -2,41 +2,17 @@ function solution(A, F, M) {
     const N = A.length;
     const expectedSum = M * (N + F);
   
-    let sumOfKnownRolls = A.reduce((acc, roll) => acc + roll, 0);
-  
+    let sumOfKnownRolls = 0;  // Start with a total of 0.
+
+    for (let i = 0; i < A.length; i++) {
+      sumOfKnownRolls = sumOfKnownRolls + A[i];  // Add each number in the array to the total.
+    }
     let sumOfMissingRolls = expectedSum - sumOfKnownRolls;
   
     if (sumOfMissingRolls < F || sumOfMissingRolls > F * 6) {
       return [0];
     }
   
-    const results = [];
-  
-    function findCombinations(remainingRolls, currentCombination, currentSum) {
-      if (remainingRolls === 0) {
-        if (currentSum === sumOfMissingRolls) {
-          results.push(currentCombination.slice());
-        }
-        return;
-      }
-  
-      for (let roll = 1; roll <= 6; roll++) {
-        if (currentSum + roll + (remainingRolls - 1) * 1 <= sumOfMissingRolls) {
-          currentCombination.push(roll);
-          findCombinations(remainingRolls - 1, currentCombination, currentSum + roll);
-          currentCombination.pop();
-        }
-      }
-    }
-  
-    findCombinations(F, [], 0);
-  
-    return results.length > 0 ? results : [0];
   }
   
-  console.log(solution([3, 2, 4, 3], 2, 4));
-  console.log(solution([1, 5, 6], 4, 3));
-  console.log(solution([1, 2, 3, 4], 4, 6));
-  console.log(solution([6, 1], 1, 1));
-  console.log(solution([6], 10, 4));
-  
+ 
